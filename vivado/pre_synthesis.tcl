adadebf8469db6fa4a4e9463db9651d8c1f20691
till straumann <till.straumann@alumni.tu-berlin.de>
2023-06-05 15:53:23 +0200

    removed unused TCL files

--- vivado/pre_synthesis.tcl
+++
@@ -1,2 +0,0 @@
-puts "PRE_SYNTHESIS USER SCRIPT"
-
--- vivado/sources.tcl
+++
@@ -1,4 +0,0 @@
-puts "USER SOURCES script"
-set_property top EcEvrProto [current_fileset]
-
-set_property generic {"FOO=BOO"} -objects [current_fileset]
